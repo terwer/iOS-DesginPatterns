d9d51af72b77d7a0e9a7052f65d51d64b25f2064
terwer <terwergreen@163.com>
2016-05-31 11:40:51 +0800

    Add Static Factory Method

---
+++ StaticFactoryMethod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StaticFactoryMethod
+//
+//  Created by Terwer Green on 16/5/31.
+//  Copyright © 2016年 Terwer Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
